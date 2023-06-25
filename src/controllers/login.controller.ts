fcc5a3f0d8c87c9f6054a79a496a8e3551dc70a4
Mihailob5017 <mihailob5017@gmail.com>
2023-06-26 00:51:41 +0200

    refactored the login component

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,7 +1,18 @@
-import { ControllerType, UserCredentialsInterface } from '../helpers/types';
-import { errorObjects, prismaClient } from '../helpers/contants';
+import {
+	ControllerType,
+	SignupResponseInterface,
+	TokenPayloadInterface,
+	UserCredentialsInterface,
+} from '../helpers/types';
+import {
+	RedisClient,
+	errorObjects,
+	prismaClient,
+	successObject,
+} from '../helpers/contants';
 import { CustomError } from '../helpers/errors';
 import bcrypt from 'bcryptjs';
+import { checkIfTokenIsValid, signJWTToken } from '../helpers/functions';
 
 export const LoginController: ControllerType = async (_req, _res) => {
 	try {
@@ -26,5 +37,42 @@ export const LoginController: ControllerType = async (_req, _res) => {
 				field: 'password',
 				message: errorObjects.passwordsDontMatch,
 			});
-	} catch (error) {}
+
+		let redisJWT = await RedisClient.get(findUser.username);
+
+		if (!redisJWT) {
+			const tokenPayload: TokenPayloadInterface = {
+				key_id: findUser.key_id || '',
+				username: findUser.username || '',
+			};
+			redisJWT = signJWTToken(tokenPayload);
+			await RedisClient.set(tokenPayload.username, redisJWT);
+		}
+
+		const tokenHasExpired = checkIfTokenIsValid(redisJWT);
+
+		if (tokenHasExpired) {
+			await RedisClient.set(findUser.username, redisJWT);
+		}
+
+		_res.status(200).json({
+			isSuccessful: true,
+			clientMessage: successObject.succesfullyLoggedIn,
+			error: null,
+			json_token: redisJWT,
+			data: {
+				...findUser,
+				solved_issues_count: Number(findUser.solved_issues_count),
+				rank: Number(findUser.rank),
+			},
+		});
+	} catch (err: any) {
+		const responseObject: SignupResponseInterface = {
+			isSuccessful: false,
+			clientMessage: errorObjects.somethingWentWrong,
+			data: null,
+			error: { ...err },
+		};
+		_res.status(400).json(responseObject);
+	}
 };
